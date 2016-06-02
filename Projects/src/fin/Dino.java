3fcab6e14a65dceab47a71d595de8ff4709eaef2
alpal7327 <huang.alpal@gmail.com>
2016-06-02 11:06:47 -0700

    Pusheenosaurus draw/flip check
    pusheen dino movement

---
+++ Projects/src/fin/Dino.java
@@ -0,0 +1,65 @@
+package fin;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class Dino extends MovingThing
+{
+	private int speed;
+	//private Image image;
+
+	public Dino()
+	{
+		this(0,0,0);
+	}
+
+	public Dino(int x, int y)
+	{
+		this(x,y,0);
+	}
+
+	public Dino(int x, int y, int s)
+	{
+		super(x, y);
+		setSpeed(s);
+/*		try
+		{
+			image = ImageIO.read(new File("src/fin/ship.jpg"));
+		}
+		catch(Exception e)
+		{
+			//feel free to do something here
+			System.out.println("Houston we have a problem");
+		}*/
+	}
+
+
+	public void setSpeed(int s)
+	{
+	   //add more code
+		speed = s;
+	}
+
+	public int getSpeed()
+	{
+	   return speed;
+	}
+
+	public void draw(Graphics window)
+	{
+		
+	}
+	
+	public void draw( Graphics window , Image image)
+	{
+		window.drawImage(image,getX(),getY(),80,80,null);
+	}
+
+	public String toString()
+	{
+		return super.toString() + "Speed: " + getSpeed();
+	}
+}
